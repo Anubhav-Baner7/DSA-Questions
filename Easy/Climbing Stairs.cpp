6e540cda3da39dfa17d9e7424936c5a10f32ff0d
Anubbhav Banerjee <88241788+Anubhav-Baner7@users.noreply.github.com>
2022-01-15 19:20:27 +0530

    Create Climbing Stairs.cpp

---
+++ Easy/Climbing Stairs.cpp
@@ -0,0 +1,27 @@
+//Problem link:https://leetcode.com/problems/climbing-stairs/
+
+//1st Appraoch(Top-down)
+int dp[46];
+    int solve(int n)
+    {
+        if(n==0)
+            return 1;
+        if(n==1)
+        return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=solve(n-1)+solve(n-2);
+    }
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+
+//2nd Appraoch(Bottom-Up)
+ int climbStairs(int n) {
+       int dp[n+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
+    }
